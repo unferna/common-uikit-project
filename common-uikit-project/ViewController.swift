c6a50219b0fafa45e025a13975097d722a02aedd
Fernando Florez <arrobaunferna@gmail.com>
2023-03-02 12:37:07 -0500

    Initial commit

---
+++ common-uikit-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  common-uikit-project
+//
+//  Created by Fernando Florez on 2/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
